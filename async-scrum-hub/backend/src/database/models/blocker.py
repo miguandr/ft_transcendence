a6e9c67fe68644d0d8ddd49c588edd92cfb2e5f6
mariaserpa <iza-serpa@hotmail.com>
2026-02-16 17:38:35 +0100

    include updated_at to main consistency and change location of test documentation

--- async-scrum-hub/backend/src/database/models/blocker.py
+++ async-scrum-hub/backend/src/database/models/blocker.py
@@ -120,6 +120,13 @@ class Blocker(Base):
 		nullable=False
 	)
 
+	updated_at: Mapped[datetime] = mapped_column(
+		DateTime(timezone=True),
+		server_default=func.now(),
+		onupdate=func.now(),
+		nullable=False
+	)
+
 	resolved_at: Mapped[datetime | None] = mapped_column(
 		DateTime(timezone=True),
 		nullable=True
